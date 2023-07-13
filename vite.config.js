9a7959b99520ae9ccb220b343809ba7709c7a301
1234 <a1234@johnWood.local>
2023-07-13 11:34:00 +0300

    setting up react router

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  base: "https://johny-wood.github.io/",
-  build: {
-    rollupOptions: {
-      sourcemapBaseUrl: "https://johny-wood.github.io/gg",
-      // https://rollupjs.org/configuration-options/
-    },
-  },
-});
+    base: process.env.VITE_BASE_URL,
+  });
+};
